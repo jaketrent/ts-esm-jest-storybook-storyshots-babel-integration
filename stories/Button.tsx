feef7385cf8856952d18e549e858e609eeb6ef62
jaketrent <trent.jake@gmail.com>
2021-03-22 14:32:55 -0600

    convert button and stories to ts

---
+++ stories/Button.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import * as css from './button.styles'
+
+export interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large'
+  /**
+   * Button contents
+   */
+  label: string
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary'
+  return (
+    <button
+      type="button"
+      style={{
+        ...css.button,
+        ...css[size],
+        ...(primary ? css.primary : css.secondary),
+        ...(backgroundColor && { backgroundColor }),
+      }}
+      {...props}
+    >
+      {label}
+    </button>
+  )
+}
